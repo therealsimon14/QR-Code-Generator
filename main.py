ca5252af426c6fc2fb4c45cb84f03aac539e3ba9
Grimet14 <gileff.simon@gmail.com>
2024-08-07 17:05:02 -0700

    added comments and restructered code, final commit

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+#import libraries
+
 from flask import Flask,render_template,request
 import qrcode
 from io import BytesIO
 from base64 import b64encode
 
+#create flask app
 app = Flask(__name__)
 
+# make flask home page render index.html file 
+# flask renders the index.html file from the "templates" folder
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# gets flask to request data from the "link" input field in my html document using POST
+# uses qrcode library to generate a qr code using the data provided and then saves the image to the computer's memory
 @app.route('/', methods=['POST'])
 def generateQR():
     memory = BytesIO()
@@ -19,7 +26,7 @@ def generateQR():
 
     base64_img = "data:image/png;base64," + \
         b64encode(memory.getvalue()).decode('ascii')
-    
+    #sends generated image back to index.html to be rendered on the webpage
     return render_template('index.html', data=base64_img)
 
 
